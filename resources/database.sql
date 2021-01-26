099a37f9768ea6b185df2de3b789036aa4375e01
QueebSkeleton <queebskeleton@gmail.com>
2021-01-26 15:44:46 +0800

    Create database structure for station pricing

--- resources/database.sql
+++ resources/database.sql
@@ -11,3 +11,15 @@ CREATE TABLE `pnr_db`.`station`(
 	description VARCHAR(90) NULL,
 	PRIMARY KEY(id)
 );
+
+CREATE TABLE station_pricing(
+	from_id INT NOT NULL,
+    to_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    FOREIGN KEY FROM_STATION_ID (from_id) REFERENCES station(id)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE,
+	FOREIGN KEY TO_STATION_ID (to_id) REFERENCES station(id)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
