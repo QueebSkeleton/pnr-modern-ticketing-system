15b77b86b753abf4f7d746e034cd1bd04ebe64cd
QueebSkeleton <queebskeleton@gmail.com>
2021-01-26 12:20:52 +0800

    Create database structure for station table

--- resources/database.sql
+++ resources/database.sql
@@ -1,4 +1,13 @@
 
-CREATE DATABASE 'pnr_db';
+CREATE DATABASE `pnr_db`;
 
 CREATE USER 'pnr_app'@'localhost' IDENTIFIED BY 'password123';
+
+GRANT ALL PRIVILEGES ON `pnr_db`.* TO 'pnr_app'@'localhost';
+
+CREATE TABLE `pnr_db`.`station`(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(45) NOT NULL,
+	description VARCHAR(90) NULL,
+	PRIMARY KEY(id)
+);
