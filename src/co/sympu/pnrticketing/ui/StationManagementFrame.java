d91a7b47c5f92a93edffc4629fa18e3edb0d57fa
QueebSkeleton <queebskeleton@gmail.com>
2021-01-26 17:22:29 +0800

    Add functionality for deleting stations

--- src/co/sympu/pnrticketing/ui/StationManagementFrame.java
+++ src/co/sympu/pnrticketing/ui/StationManagementFrame.java
@@ -5,6 +5,9 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -15,6 +18,8 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+
+import co.sympu.pnrticketing.util.DatabaseUtility;
 
 /**
  * Station Management Panel.
@@ -153,6 +158,63 @@ public class StationManagementFrame extends JFrame {
 		/* jbtnDelete - Button for removing a station */
 		JButton jbtnDelete = new JButton("Delete");
 		jbtnDelete.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		
+		// Delete Button Click Event
+		// When this button is clicked, delete the selected row on the table
+		jbtnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// If there are no selected rows in the table
+				if(jtblStation.getSelectionModel().getSelectedItemsCount() == 0) {
+					// Output a friendly message telling the user to select a row first
+					JOptionPane.showMessageDialog(
+							thisFrame,
+							"Please select a station first before clicking the delete button.",
+							"Notice",
+							JOptionPane.WARNING_MESSAGE);
+					return;
+				}
+				
+				// Else, grab the selected row index
+				int selectedRowIndex = jtblStation.getSelectionModel().getSelectedIndices()[0];
+				
+				// Get the id of the station in the table model depending on the given row
+				int stationId = stationTableModel.getStationId(selectedRowIndex);
+				
+				// TODO: Start database transaction here
+				try(
+					// Grab a connection to the database
+					Connection connection = DatabaseUtility.dataSource.getConnection();
+					// Create a delete station statement holder
+					PreparedStatement deleteStationStatement = connection.prepareStatement("DELETE FROM station WHERE id = ?")) {
+					
+					// Bind the station id to the delete statement
+					deleteStationStatement.setInt(1, stationId);
+					
+					// Execute the delete
+					deleteStationStatement.execute();
+				} catch(SQLException exception) {
+					// In the case where anything above causes an SQLException,
+					// output a message stating that the delete failed. Prompt the user to try again.
+					JOptionPane.showMessageDialog(
+							thisFrame,
+							"Delete failed. Please try again later.",
+							"Notice",
+							JOptionPane.WARNING_MESSAGE);
+					return;
+				}
+				
+				// When execution reaches here,
+				// the delete was successful. Output a friendly message.
+				JOptionPane.showMessageDialog(
+						thisFrame,
+						"Successfully removed the station.",
+						"Success!",
+						JOptionPane.INFORMATION_MESSAGE);
+				
+				// Refresh the table.
+				stationTableModel.refresh();
+			}
+		});
 		jpnlButtonActions.add(jbtnDelete);
 		/* END OF jbtnDelete */
 		
