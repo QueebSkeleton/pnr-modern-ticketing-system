ed21800a1753c86e71e2a768f626f8508bd0901a
ProfessorEL <60186871+ProfessorEL-psd@users.noreply.github.com>
2021-02-11 01:59:39 +0800

    Deleted unnecessary imports

--- src/co/sympu/pnrticketing/ui/ticketerrorhandling/TicketCashierPrompt.java
+++ src/co/sympu/pnrticketing/ui/ticketerrorhandling/TicketCashierPrompt.java
@@ -1,29 +1,28 @@
 package co.sympu.pnrticketing.ui.ticketerrorhandling;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.border.EmptyBorder;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
-import java.awt.Dimension;
-import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
-import javax.swing.border.EtchedBorder;
-import java.awt.Color;
-import javax.swing.border.BevelBorder;
 import javax.swing.SwingConstants;
-import java.awt.Cursor;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
 
 public class TicketCashierPrompt extends JFrame {
 
